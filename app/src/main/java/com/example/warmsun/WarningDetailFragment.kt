c099a858849df44492fc3698edbc6d4e1482c709
haojinhui <3526250846@qq.com>
2023-08-05 20:18:38 +0800

    添加预警提示界面

---
+++ app/src/main/java/com/example/warmsun/WarningDetailFragment.kt
@@ -0,0 +1,23 @@
+package com.example.warmsun
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.warmsun.databinding.FragmentWarningDetailBinding
+
+class WarningDetailFragment : Fragment() {
+
+    private lateinit var binding: FragmentWarningDetailBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentWarningDetailBinding.inflate(inflater, container, false)
+
+
+
+        return binding.root
+    }
+}
