bb89bb6b3896c315514cbb0862cc619eb36b34cb
haojinhui <3526250846@qq.com>
2023-08-05 14:32:58 +0800

    添加主要fragment

---
+++ app/src/main/java/com/example/warmsun/FragmentType.kt
@@ -0,0 +1,8 @@
+package com.example.warmsun
+
+enum class FragmentType(name: String) {
+    HOME("home"),
+    MAIN("main"),
+    MESSAGE("message"),
+    MY("my")
+}
--- app/src/main/java/com/example/warmsun/MainActivity.kt
+++ app/src/main/java/com/example/warmsun/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.warmsun
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.warmsun.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val fragmentMap = mutableMapOf<String, Fragment>()
+
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        showFragment(FragmentType.MAIN)
+    }
+
+    fun showFragment(type: FragmentType) {
+        val transaction = supportFragmentManager.beginTransaction()
+        // 隐藏所有
+        fragmentMap.forEach { (name, fragment) ->
+            transaction.hide(fragment)
+        }
+        // 没有，添加到map中
+        if (type.name !in fragmentMap) {
+            fragmentMap[type.name] = when (type) {
+                FragmentType.HOME -> HomeFragment()
+                FragmentType.MAIN -> MainFragment()
+                FragmentType.MESSAGE -> MessageFragment()
+                FragmentType.MY -> MyFragment()
+            }
+            transaction.add(binding.frameLayout.id, fragmentMap[type.name]!!)
+        } else {
+            // 有显示
+            transaction.show(fragmentMap[type.name]!!)
+        }
+        transaction.commit()
     }
 }
